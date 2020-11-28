1f6cde9af32ad76d285b8b0f906020ce83ee35ad
Daylam Tayari <58621925+daylamtayari@users.noreply.github.com>
2020-11-27 18:11:06 -0700

    Created the urlCompute method
    Created the urlCompute method.
    Created the domain file which tracks Twitch server VOD domains.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class Main {
+	public static void main(String[] args) {
+		
+	}
 	
 	/**
 	 * Computes the SHA1 hash of the given final string and returns the first 20 values.
@@ -9,9 +12,9 @@ public class Main {
 	 * @return String		Returns the first 20 values of the SHA1 hash of the given string.
 	 * @throws NoSuchAlgorithmException
 	 */
-	public String hash(String finalString) throws NoSuchAlgorithmException {
+	public String hash(String baseString) throws NoSuchAlgorithmException {
 		MessageDigest md=MessageDigest.getInstance("SHA1");
-		byte[] result=md.digest(finalString.getBytes());
+		byte[] result=md.digest(baseString.getBytes());
 		StringBuffer sb=new StringBuffer();
 		for(int i=0; i<result.length; i++) {
 			sb.append(Integer.toString((result[i] & 0xff)+0x100, 16).substring(1));
@@ -19,4 +22,25 @@ public class Main {
 		String hash=sb.toString();
 		return hash.substring(0, 20);
 	}
+	
+	/**
+	 * Computes the URl (excluding the domain itself) and calls the hashing method.
+	 * @param name			Streamer name.
+	 * @param vodID			VOD ID.
+	 * @param timestamp		UNIX Timestamp.
+	 * @return String		URL of the VOD excluding the actual domain.
+	 */
+	public String urlCompute(String name, int vodID, int timestamp) {
+		String baseString=name+"_"+Integer.toString(vodID)+"_"+Integer.toString(timestamp);
+		try {
+			String hash=hash(baseString);
+			String finalString=hash+"_"+baseString;
+			return "/"+finalString+"/chunked/index-dvr.m3u8";
+		}
+		catch(NoSuchAlgorithmException e){
+			return "NoSuchAlgorithmException error"+e;
+		}
+	}
+	
+	
 }
