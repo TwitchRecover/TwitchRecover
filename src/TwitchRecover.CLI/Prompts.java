7839974b4f61cafea14d54108ef4001d6c623717
Daylam Tayari <58621925+daylamtayari@users.noreply.github.com>
2020-12-14 20:24:44 -0700

    CLI menu prompt options
    Completed the CLI menu prompt options.

--- src/TwitchRecover.CLI/Prompts.java
+++ src/TwitchRecover.CLI/Prompts.java
@@ -16,10 +16,64 @@
 
 package TwitchRecover.CLI;
 
+import java.util.Scanner;
+
 /**
  * This class holds all of the prompts
  * and the prompt handlers for the
  * CLI version of the program.
  */
 public class Prompts {
+    /**
+     * This prints the welcome prompt.
+     */
+    protected static void welcome(){
+        System.out.print(
+                  "\nWelcome to Twitch Recover!"
+                + "\nThe number one Twitch recovery tool created by Daylam Tayari (https://github.com/daylamtayari)"
+                + "\nIf you like this tool please help support me: https://paypal.me/daylamtayari https://cash.app/$daylamtayari"
+                + "\n"
+        );
+    }
+
+    /**
+     * This method prints the main menu
+     * and returns the option the user selected.
+     * @return int  Integer value representing the option that the user selected.
+     */
+    protected static int menu(){
+        Scanner sc=new Scanner(System.in);
+        menuPrinter();
+        System.out.print("\nPlease enter the number of the option you want to select (number between 1-9 inclusive:\n");
+        int input=Integer.parseInt(sc.nextLine());
+        while(!(input>0 && input<10)){
+            System.out.print(
+                    "\n\nINCORRECT INPUT"
+                    + "\nPLEASE ENTER THE NUMBER OF THE OPTION YOU WANT TO SELECT (number between 1-9 inclusive):\n"
+            );
+            input=Integer.parseInt(sc.nextLine());
+        }
+        return input;
+    }
+
+    /**
+     * This method prints the main menu options.
+     */
+    private static void menuPrinter(){
+        System.out.print(
+                "\nMenu:"
+                        + "\nVODs:"
+                        + "\n1. Download a VOD."
+                        + "\n2. Download a sub-only VOD (without being subbed to that channel)."
+                        + "\n3. Recover a VOD - 60 days maximum."
+                        + "\n4. View a muted M3U8 VOD - If a M3U8 VOD has muted segments, use this to view it in its entirety."
+                        + "\nHighlights:"
+                        + "\n5. Download a highlight."
+                        + "\n6. Recover a highlight."
+                        + "\nClips:"
+                        + "\n7. Download a clip."
+                        + "\n8. Recover all clips from a stream - NO time limit."
+                        + "\n9. Mass recover options:"
+        );
+    }
 }
