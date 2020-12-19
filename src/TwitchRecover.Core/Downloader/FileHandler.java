cad399b273676110266590e3dbc47a64d865ba36
Daylam Tayari <58621925+daylamtayari@users.noreply.github.com>
2020-12-19 08:17:26 -0700

    Created the FileHandler class and temp folder creation
    - Created the FileHandler class which handles the files for the download package.
    - Created the createTempFolder() class which creates a temp folder.

---
+++ src/TwitchRecover.Core/Downloader/FileHandler.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2020 Daylam Tayari <daylam@tayari.gg>
+ *
+ * This library is free software. You can redistribute it and/or modify it under the terms of the GNU General Public License version 3 as published by the Free Software Foundation.
+ * This program is distributed in the that it will be use, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If not see http://www.gnu.org/licenses/ or write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+/*
+ * @author Daylam Tayari https://github.com/daylamtayari
+ * @version 2.0
+ * Github project home page: https://github.com/TwitchRecover
+ * Twitch Recover repository: https://github.com/TwitchRecover/TwitchRecover
+ */
+
+package TwitchRecover.Core.Downloader;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+/**
+ * This class handles all of the file handling
+ * for the Download package.
+ */
+class FileHandler {
+    protected static Path TEMP_FOLDER_PATH;    //Variable which holds the folder path of the temp folder.
+
+    /**
+     * This method creates a temp folder where all of the temporary TS
+     * files (M3U8 parts) will be saved.
+     * @throws IOException
+     */
+    protected static void createTempFolder() throws IOException {
+        TEMP_FOLDER_PATH= Files.createTempDirectory("TwitchRecover-").toAbsolutePath();
+    }
+}
