4431ae91e97e31370e1b00ab29f5f9352494d5ec
Daylam Tayari <58621925+daylamtayari@users.noreply.github.com>
2020-12-19 09:14:10 -0700

    Created the Download class and completed tempDownload method
    - Created the Download class which holds all of the downloading methods.
    - Completed the tempDownload method which creates a temporary file in the temporary download folder.

---
+++ src/TwitchRecover.Core/Downloader/Download.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2020 Daylam Tayari <daylam@tayari.gg>
+ *
+ * This library is free software. You can redistribute it and/or modify it under the terms of the GNU General Public License version 3 as published by the Free Software Foundation.
+ * This program is distributed in the that it will be use, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If not see http://www.gnu.org/licenses/ or write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+/*
+ * @author Daylam Tayari https://github.com/daylamtayari
+ * @version 2.0
+ * Github project home page: https://github.com/TwitchRecover
+ * Twitch Recover repository: https://github.com/TwitchRecover/TwitchRecover
+ */
+
+package TwitchRecover.Core.Downloader;
+
+import TwitchRecover.Core.Enums.Timeout;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+/**
+ * This class holds all of the downloading methods
+ * and handles all of the downloads.
+ */
+public class Download {
+
+    /**
+     * This method creates a temporary download
+     * from a URL.
+     * @param url       URL of the file to be downloaded.
+     * @return File     File object of the file that will be downloaded and is returned.
+     * @throws IOException
+     */
+    protected static File tempDownload(String url) throws IOException{
+        URL dURL=new URL(url);
+        String prefix=FilenameUtils.getBaseName(dURL.getPath());
+        if(prefix.length()<10){     //This has to be implemented since the prefix value of the createTempFile method
+            prefix="00"+prefix;     //which we use to create a temp file, has to be a minimum of 3 characters long.
+        }
+        else if(prefix.length()<100){
+            prefix="0"+prefix;
+        }
+        File downloadedFile=File.createTempFile(FileHandler.TEMP_FOLDER_PATH+File.separator+prefix, FilenameUtils.getExtension(dURL.getPath()));    //Creates the temp file.
+        downloadedFile.deleteOnExit();
+        FileUtils.copyURLToFile(dURL, downloadedFile, Timeout.CONNECT.time, Timeout.READ.time);
+        return downloadedFile;
+    }
+}
