7c53a936d7167b9f77f87167dd5a2db0d575f708
Daylam Tayari <58621925+daylamtayari@users.noreply.github.com>
2020-12-19 13:41:34 -0700

    Completed initial draft of the TSDownload method
    Completed the initial draft of the TSDownload method which downloads all of the individual TS files of an M3U8.

--- src/TwitchRecover.Core/Downloader/Download.java
+++ src/TwitchRecover.Core/Downloader/Download.java
@@ -16,12 +16,21 @@
 
 package TwitchRecover.Core.Downloader;
 
+import TwitchRecover.Core.Enums.FileExtensions;
 import TwitchRecover.Core.Enums.Timeout;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.NavigableMap;
+import java.util.Queue;
+import java.util.TreeMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * This class holds all of the downloading methods
@@ -42,7 +51,9 @@ public class Download {
         FileUtils.copyURLToFile(dURL, dFile, Timeout.CONNECT.time, Timeout.READ.time);
     }
 
-    public static void m3u8Download(String url, String fp){
+    public static void m3u8Download(String url, String fp) throws IOException {
+        FileHandler.createTempFolder();
+        ArrayList<String> chunks=M3U8Handler.getChunks(url);
 
     }
 
@@ -62,9 +73,42 @@ public class Download {
         else if(prefix.length()<100){
             prefix="0"+prefix;
         }
-        File downloadedFile=File.createTempFile(FileHandler.TEMP_FOLDER_PATH+File.separator+prefix, FilenameUtils.getExtension(dURL.getPath()));    //Creates the temp file.
+        File downloadedFile=File.createTempFile(FileHandler.TEMP_FOLDER_PATH+File.separator+prefix, "."+FilenameUtils.getExtension(dURL.getPath()));    //Creates the temp file.
         downloadedFile.deleteOnExit();
         FileUtils.copyURLToFile(dURL, downloadedFile, Timeout.CONNECT.time, Timeout.READ.time);
         return downloadedFile;
+    }
+
+    private static NavigableMap<Integer, File> TSDownload(ArrayList<String> links, String fp){
+        NavigableMap<Integer, File> segmentMap=new TreeMap<>();
+        Queue<String> downloadQueue=new ConcurrentLinkedQueue<>();
+        for(String link: links){    //Adds all the links to the ressource queue.
+            downloadQueue.offer(link);
+        }
+        int index=0;
+        ThreadPoolExecutor downloadTPE=(ThreadPoolExecutor) Executors.newFixedThreadPool(100);
+        while(!downloadQueue.isEmpty()){
+            index++;
+            String item=downloadQueue.poll();
+            final int finalIndex=index;
+            downloadTPE.execute(new Runnable() {
+                @Override
+                public void run() {
+                    final int threadIndex=finalIndex;
+                    final String threadItem=item;
+                    try{
+                        File tempTS=tempDownload(threadItem);
+                        segmentMap.put(threadIndex, tempTS);
+                    }
+                    catch(Exception ignored){}
+                }
+            });
+        }
+        downloadTPE.shutdown();
+        try{
+            downloadTPE.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        }
+        catch(Exception ignored){}
+        return segmentMap;
     }
 }
