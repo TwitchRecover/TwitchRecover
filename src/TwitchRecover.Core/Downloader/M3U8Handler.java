b1f7fa3793a1b924bc2f733ac920fdeb1852a048
Daylam Tayari <58621925+daylamtayari@users.noreply.github.com>
2020-12-19 11:31:11 -0700

    Completed the getChunks method
    Completed the getChunks method which retrieves all of the chunks of an M3U8 URL.

--- src/TwitchRecover.Core/Downloader/M3U8Handler.java
+++ src/TwitchRecover.Core/Downloader/M3U8Handler.java
@@ -16,10 +16,49 @@
 
 package TwitchRecover.Core.Downloader;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * This class handles all of the methods related
  * to M3U8 processing for downloads.
  */
 class M3U8Handler {
-
+    /**
+     * This method retrieves all of the
+     * chunks of an M3U8 URL.
+     * @param url                   URL of the M3U8 file to retrieve the chunks off.
+     * @return ArrayList<String>    String arraylist containing all of the chunks of the M3U8 file.
+     * @throws IOException
+     */
+    protected static ArrayList<String> getChunks(String url) throws IOException {
+        ArrayList<String> chunks=new ArrayList<String>();
+        String baseURL="";
+        String pattern = "([a-zA-Z0-9]*\\.cloudfront\\.net\\/[a-zA-Z0-9_]*\\/[a-zA-Z0-9_]*\\/)index-dvr\\.m3u8";
+        Pattern r = Pattern.compile(pattern);
+        Matcher m = r.matcher(url);
+        if(m.find()){
+            baseURL="https://"+m.group(1);
+        }
+        File m3u8File=Download.tempDownload(url);
+        Scanner sc=new Scanner(m3u8File);
+        String line;
+        while(sc.hasNextLine()){
+            line=sc.nextLine();
+            if(line.contains("unmuted")){
+                chunks.add(baseURL+line.substring(0, line.indexOf("-")+1)+"muted.ts");
+            }
+            else{
+                chunks.add(baseURL+line);
+            }
+        }
+        return chunks;
+    }
 }
