1395f0fcef094f817d52066c7b2d354bff085218
Daylam Tayari <58621925+daylamtayari@users.noreply.github.com>
2020-12-19 08:55:45 -0700

    Created the Timeout enums
    Created the Timeout enums which handles the timeout duration for connections, read, etc.

---
+++ src/TwitchRecover.Core/Enums/Timeout.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2020 Daylam Tayari <daylam@tayari.gg>
+ *
+ * This library is free software. You can redistribute it and/or modify it under the terms of the GNU General Public License version 3 as published by the Free Software Foundation.
+ * This program is distributed in the that it will be use, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If not see http://www.gnu.org/licenses/ or write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package TwitchRecover.Core.Enums;
+
+public enum Timeout {
+    CONNECT(60000),
+    READ(60000);
+
+    public int time;    //Timeout time in milliseconds.
+    Timeout(int m){
+        time=m;
+    }
+}
