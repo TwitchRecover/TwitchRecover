a223e061b13b3bb0895be2ba4c7b21e78054e7be
Daylam Tayari <58621925+daylamtayari@users.noreply.github.com>
2020-12-23 04:09:52 -0700

    Created the API class
    Created the API class which handles all API calls.

---
+++ src/TwitchRecover.Core/API.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2020 Daylam Tayari <daylam@tayari.gg>
+ *
+ * This library is free software. You can redistribute it and/or modify it under the terms of the GNU General Public License version 3 as published by the Free Software Foundation.
+ * This program is distributed in the that it will be use, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If not see http://www.gnu.org/licenses/ or write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+/*
+ * @author Daylam Tayari https://github.com/daylamtayari
+ * @version 2.0
+ * Github project home page: https://github.com/TwitchRecover
+ * Twitch Recover repository: https://github.com/TwitchRecover/TwitchRecover
+ */
+
+package TwitchRecover.Core;
+
+/**
+ * This class handles all of the
+ * API calls and retrievals.
+ */
+public class API {
+}
