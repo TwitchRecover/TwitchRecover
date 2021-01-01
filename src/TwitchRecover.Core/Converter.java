8968a34b4f16152dd049e739729f947f1a058cde
Daylam Tayari <58621925+daylamtayari@users.noreply.github.com>
2021-01-01 10:24:21 -0700

    Completed the Converter object class
    Completed the Converter object class which handles the conversion of a TS file to another file extension.

---
+++ src/TwitchRecover.Core/Converter.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2021 Daylam Tayari <daylam@tayari.gg>
+ *
+ * This library is free software. You can redistribute it and/or modify it under the terms of the GNU General Public License version 3 as published by the Free Software Foundation.
+ * This program is distributed in the that it will be use, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If not see http://www.gnu.org/licenses/ or write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+/*
+ * @author Daylam Tayari https://github.com/daylamtayari
+ * @version 2.0
+ * Github project home page: https://github.com/TwitchRecover
+ * Twitch Recover repository: https://github.com/TwitchRecover/TwitchRecover
+ */
+
+package TwitchRecover.Core;
+
+import TwitchRecover.Core.Enums.FileExtension;
+
+import java.io.File;
+
+/**
+ * Converter object class
+ * which converts a TS file to
+ * another file extension.
+ */
+public class Converter {
+    private String ogFP;
+    private FileExtension fe;
+    private String rawFP;
+
+    /**
+     * Constructor for the Converter object.
+     * @param ogFP  String value representing the complete original file path of the file to convert.
+     * @param fe    FileExtension enum value representing the output desired file extension.
+     */
+    public Converter(String ogFP, FileExtension fe){
+        this.ogFP=ogFP;
+        this.fe=fe;
+    }
+
+    /**
+     * Main method for the Converter object class.
+     */
+    public void main(){
+        if(!checkTS()){
+            System.out.println("ERROR: File is not a TS file.");
+            return;
+        }
+        getRaw();
+        File ogFile=new File(ogFP);
+        File newFile=new File(rawFP+fe.fileExtension);
+        ogFile.renameTo(newFile);
+    }
+
+    /**
+     * This gets the raw file path
+     * from the given file path.
+     */
+    private void getRaw(){
+        rawFP=ogFP.substring(0, ogFP.lastIndexOf('.'));
+    }
+
+    /**
+     * This checks if the inputted file is
+     * indeed a TS file.
+     * @return Boolean  Boolean value representing whether or not the file is a TS file.
+     */
+    private boolean checkTS(){
+        if(ogFP.substring(ogFP.lastIndexOf(".")).toLowerCase().equals(".ts")){
+            return true;
+        }
+        return false;
+    }
+}
