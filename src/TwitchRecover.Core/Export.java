7397b8a001353cf4f7425b87c5473819fbb78b0e
Daylam Tayari <58621925+daylamtayari@users.noreply.github.com>
2021-01-19 04:32:13 -0700

    Completed Exports object class
    Completed the Exports object class which handles the exporting of results.

--- src/TwitchRecover.Core/Export.java
+++ src/TwitchRecover.Core/Export.java
@@ -17,6 +17,9 @@
 
 package TwitchRecover.Core;
 
+import TwitchRecover.Core.Enums.ContentType;
+import TwitchRecover.Core.Enums.FileExtension;
+
 import java.util.ArrayList;
 
 /**
@@ -26,6 +29,11 @@ import java.util.ArrayList;
  */
 public class Export {
     private ArrayList<String> results;      //String arraylist which contains all of the results to export.
+    private String fp;                      //String variable representing the file path of where to store the results.
+    private String fFP;                     //String variable representing the final file path, including the file extension, of where the results will be exported.
+    private ContentType ct;                 //Content type variable which represents what type of content the results are from.
+    private String id;                      //String variable representing the ID of the content in question.
+
     /**
      * Constructor for the export object
      * class which initiates the object
@@ -36,5 +44,69 @@ public class Export {
         results.add("# Results generated using Twitch Recover - https://github.com/twitchrecover/twitchrecover");
         results.add("# Please consider donating if this has been useful for you - https://paypal.me/daylamtayari https://cash.app/$daylamtayari BTC: 15KcKrsqW6DQdyZPrgRXXmsKkyyZzHAQVX");
         results.add("#-----------------------------------------------------------------------------------------------------------------------------------------------------------------");
+    }
+
+    /**
+     * Export method which exports
+     * the results to the desired file.
+     */
+    public void export(){
+        computeFN();
+        FileIO.write(results, fFP);
+    }
+
+    /**
+     * Adds a result value to the results list.
+     * @param result    String value representing the result value to add to the results list.
+     */
+    public void addResult(String result){
+        results.add(result);
+    }
+
+    /**
+     * Adds a comment to the results list.
+     * @param comment   String value representing a comment to add to the results list.
+     */
+    public void addComment(String comment){
+        results.add("# "+comment);
+    }
+
+    /**
+     * Mutator for the file path variable.
+     * @param fp    String variable which represents the file path of where to store the exported results.
+     * @param ct    ContentType enum representing what type of content the results are for.
+     * @param id    String value representing the ID of the content the results are for. Can be null.
+     */
+    public void setFP(String fp, ContentType ct, String id){
+        this.fp=FileIO.adjustFP(fp);
+        this.ct=ct;
+        this.id=id;
+    }
+
+    /**
+     * Accessor for the fFP variable.
+     * @return String   String variable which represents the final file path of where the file will be stored.
+     */
+    public String getFFP(){
+        return fFP;
+    }
+
+    /**
+     * This computes the final file name of the output file.
+     */
+    private void computeFN(){
+        computeID();
+        fFP=fp+"TwitchRecover-"+ct.toString()+"-Results-"+id+ FileExtension.TXT.getFE();
+    }
+
+    /**
+     * This computes the ID value of
+     * the file if the ID value is
+     * currently null.
+     */
+    private void computeID(){
+        if(id==null){
+            id=String.valueOf((int) (Math.random()*1000000));
+        }
     }
 }
