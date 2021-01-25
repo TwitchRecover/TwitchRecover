8159b6225482041a88d93a131be5e698aa9e0f6b
Daylam Tayari <58621925+daylamtayari@users.noreply.github.com>
2021-01-25 09:03:10 -0700

    Created and completed the VODInfo object class
    Created and completed the VODInfo object class which will replace the vodInfo string arrays usually used in the other object classes to hold the stream information.

---
+++ src/TwitchRecover.Core/VODInfo.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright (c) 2020, 2021 Daylam Tayari <daylam@tayari.gg>
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License version 3as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not see http://www.gnu.org/licenses/ or write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ *  @author Daylam Tayari daylam@tayari.gg https://github.com/daylamtayari
+ *  @version 2.0b
+ *  Github project home page: https://github.com/TwitchRecover
+ *  Twitch Recover repository: https://github.com/TwitchRecover/TwitchRecover
+ */
+
+package TwitchRecover.Core;
+
+/**
+ * Object class which conatains
+ * the VOD information.
+ */
+public class VODInfo {
+    private String channelName;     //String value representing the channel name.
+    private long streamID;          //Long value representing the stream ID.
+    private String timestamp;       //String value representing the timestamp in YYYY-MM-DD HH:mm:ss format.
+    private long unixTimestamp;     //Long value representing the UNIX timestamp.
+    private boolean bruteForce;     //Boolean value representing whether or not the last minute should be brute forced.
+
+    /**
+     * Constructor of the object class
+     * which instantiates a new
+     * VOD Info object.
+     */
+    public VODInfo(){
+    }
+
+    //Mutators:
+
+    /**
+     * Mutator for the channelName variable.
+     * @param name  String value representing the channel name.
+     */
+    public void setName(String name){
+        channelName=name;
+    }
+
+    /**
+     * Mutator for the streamID variable.
+     * @param ID    Long value representing the stream ID.
+     */
+    public void setID(long ID){
+        streamID=ID;
+    }
+
+    /**
+     * Mutator for the streamID variable from a string value.
+     * @param ID    String value representing the stream ID.
+     */
+    public void setIDS(String ID){
+        streamID=Long.parseLong(ID);
+    }
+
+    /**
+     * Mutator for the string timestamp variable.
+     * @param ts    String value representing the start timestamp.
+     */
+    public void setTimestamp(String ts){
+        timestamp=ts;
+    }
+
+    /**
+     * Mutator for the UNIX timestamp variable.
+     * @param unix  Long value representing the UNIX timestamp.
+     */
+    public void setUnix(long unix){
+        unixTimestamp=unix;
+    }
+
+    /**
+     * Mutator for the brute force boolean.
+     * @param BF    Boolean value representing whether or not to brute force to the minute.
+     */
+    public void setBF(boolean BF){
+        bruteForce=BF;
+    }
+
+    //Accessors:
+
+    /**
+     * Accessor for the channelName variable.
+     * @return String   String value representing the channel name.
+     */
+    public String getName(){
+        return channelName;
+    }
+
+    /**
+     * Accessor for the stream ID variable.
+     * @return Long     Long value representing the stream ID.
+     */
+    public long getID(){
+        return streamID;
+    }
+
+    /**
+     * Accessor for the UNIX timestamp variable, and computes it if it the value has not been computed yet.
+     * @return Long     Value of the UNIX timestamp.
+     */
+    public long getTS(){
+        if(unixTimestamp==0L){
+            unixTimestamp=Compute.getUNIX(timestamp);
+        }
+        return unixTimestamp;
+    }
+
+    /**
+     * Accessor for the string value of the timestamp.
+     * @return String   String value of the timestamp value.
+     */
+    public String getSTS(){
+        return timestamp;
+    }
+
+    /**
+     * Accessor for the brute force boolean value.
+     * @return boolean  Boolean value for the brute force boolean.
+     */
+    public boolean getBF(){
+        return bruteForce;
+    }
+}
