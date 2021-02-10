2eb1e475abed7f7fe41cfb031754a633a19bb103
Daylam Tayari <58621925+daylamtayari@users.noreply.github.com>
2021-02-10 08:34:27 -0700

    Created the VideoType enum
    Created the VideoType enum which represents all of the officially supported broadcast types.

---
+++ src/TwitchRecover.Core/Enums/VideoType.java
@@ -0,0 +1,16 @@
+package TwitchRecover.Core.Enums;
+
+/**
+ * Enum which represents all of the officially
+ * supported broadcast types.
+ */
+public enum VideoType {
+    ARCHIVE,
+    HIGHLIGHT,
+    UPLOAD,
+    PREMIERE_UPLOAD,
+    PAST_PREMIERE;
+
+    VideoType(){
+    }
+}
